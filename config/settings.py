40dc005870b3fafdb4b37fae522b63a9245a2e2d
Seo Jun Hyeok <46551990+taulmax@users.noreply.github.com>
2020-09-06 18:23:15 +0900

    settings.py에 MySQL 설정을 했습니다.
    settings.py에 MySQL 설정을 했습니다.

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +74,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+pymysql.version_info = (1, 4, 2, "final", 0)
+pymysql.install_as_MySQLdb()
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'onlineshop.cwutxfgwcvqa.ap-northeast-2.rds.amazonaws.com',
+        'NAME': 'onlineshop',
+        'USER': 'admin',
+        'PASSWORD': 'admin12345',
+        'PORT': '3306',
     }
 }
 
